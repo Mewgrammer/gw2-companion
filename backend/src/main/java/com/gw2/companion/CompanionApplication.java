c97f6ac59385f7680e4628734a51e46c5c9c008f
Michael.Mew <xtremekrotas@gmail.com>
2018-06-12 23:36:22 +0200

    Initial commit

---
+++ backend/src/main/java/com/gw2/companion/CompanionApplication.java
@@ -0,0 +1,12 @@
+package com.gw2.companion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanionApplication.class, args);
+	}
+}
