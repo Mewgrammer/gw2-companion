6f4c62b37005cc6485afb51712299f539665a423
Michael.Mew <xtremekrotas@gmail.com>
2018-06-21 10:40:16 +0200

    dockerized backend

---
+++ backend/src/main/java/exceptions/CustomHttpException.java
@@ -0,0 +1,22 @@
+package exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class CustomHttpException extends Exception {
+
+    private HttpStatus statusCode;
+
+    public CustomHttpException(String reason, HttpStatus statuscode) {
+        super(reason);
+        this.statusCode = statuscode;
+    }
+
+    public HttpStatus getStatusCode() {
+        return statusCode;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("HTTP Exception\nStatus: %s\nMessage:%s", statusCode, getMessage());
+    }
+}
